84f9bf7c2b1b912fcbcb558462e01d9786537543
louysotopo <louysotopo123@gmail.com>
2021-07-17 16:27:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, app
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return '<h1> SSL certificado </h1>'
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
