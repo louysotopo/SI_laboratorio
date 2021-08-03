5dc4d2d88e9b84ee132ceeb6761f069db7c0d41e
louysotopo <louysotopo123@gmail.com>
2021-08-03 09:57:15 -0500

    fgfgf

--- app.py
+++ app.py
@@ -7,9 +7,5 @@ app = Flask(__name__)
 def index():
     return '<h1> SSL certificado </h1>'
     
-@app.route('/.well-known/pki-validation/2B629DA0F08747ECBE5DA8DC53E1D04D.txt')
-def hello():
-    return send_file(path_or_file='2B629DA0F08747ECBE5DA8DC53E1D04D.txt')
-
 if __name__ == '__main__':
     app.run(debug=False)
