84433857e1d6a103ca555fe5f67d918e2d000b91
louysotopo <louysotopo123@gmail.com>
2021-08-03 10:07:00 -0500

    asd

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
-from flask import Flask, app
-from flask.helpers import url_for,send_file
+from flask import Flask, jsonify;
+from flask_cors import CORS;
 
 app = Flask(__name__)
-
-@app.route('/')
+CORS(app)
+weather = {
+"data": [
+    {
+        "day": "1/6/2019",
+        "temperature": "23",
+        "windspeed": "16",
+        "event": "Sunny"
+    },
+    {
+        "day": "2/6/2019",
+        "temperature": "21",
+        "windspeed": "18",
+        "event": "Rainy"
+    },
+    {
+        "day": "3/6/2019",
+        "temperature": "31",
+        "windspeed": "12",
+        "event": "Sunny"
+    },
+    {
+        "day": "4/6/2019",
+        "temperature": "5",
+        "windspeed": "28",
+        "event": "Snow"
+    },
+    {
+        "day": "5/6/2019",
+        "temperature": "17",
+        "windspeed": "18",
+        "event": "Rainy"
+    },
+    {
+        "day": "6/6/2019",
+        "temperature": "19",
+        "windspeed": "21",
+        "event": "Rainy"
+    },
+    {
+        "day": "7/6/2019",
+        "temperature": "28",
+        "windspeed": "14",
+        "event": "Sunny"
+    }
+    ]
+}
+@app.route("/", methods=['GET'])
 def index():
-    return '<h1> SSL certificado </h1>'
-    
+    return "HOLA MUNDO"
+
+@app.route("/report/",methods=['GET'] )
+def reportWeather():
+    global weather
+    return jsonify([weather])
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
+
